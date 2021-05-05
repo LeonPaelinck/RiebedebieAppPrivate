0e2b658e4445dba984fcef11844c653ceccdb5df
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-05-05 23:22:53 +0200

    Reservatie backend toegevoegd (stap 1)

--- Assignment/Server/Api/Models/Child.cs
+++ Assignment/Server/Api/Models/Child.cs
@@ -16,6 +16,26 @@ namespace RiebedebieApi.Models
         [Required]
         public DateTime BirthDate { get; set; }
 
+        public int Age { get { return DateTime.Now.Year - BirthDate.Year; } }
+
+        public AgeCategory AgeCategory 
+        { 
+            get 
+            {
+                if (Age < 3)
+                   return AgeCategory.Peuter;
+                else if (this.Age < 6)
+                   return AgeCategory.Kleuter;
+                else if (this.Age < 12)
+                   return AgeCategory.Kind;
+                else if (this.Age < 16)
+                   return AgeCategory.Tiener;
+                else
+                   return AgeCategory.Animator;
+            }
+        }
+
+
         public Child()
         {
 
---
+++ Assignment/Server/Api/Models/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RiebedebieApi.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public Child Child;
+        public DateTime Date { get; set; }
+        public bool Earlier { get; set; }
+        public bool Later { get; set; }
+        public decimal PricePerDay { get; set; }
+        public decimal AfterHoursPrice { get; set; } 
+        public decimal Price
+        {
+            get
+            {
+                decimal price = PricePerDay;
+                if (Earlier) price += AfterHoursPrice;
+                if (Later) price += AfterHoursPrice;
+                return price;
+            }
+        }
+
+
+    }
+}
