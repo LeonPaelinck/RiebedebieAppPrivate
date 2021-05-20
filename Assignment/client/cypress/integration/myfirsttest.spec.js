5ed76318a46b1c83408468eefbed35b4ac447d80
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-05-20 17:56:50 +0200

    cypress toevoegen

---
+++ Assignment/client/cypress/integration/myfirsttest.spec.js
@@ -0,0 +1,8 @@
+describe('My First Test', function() {
+    it('Does not do much!', function() {
+      expect(true).to.equal(true);
+    });
+    it('Still not doing a lot', function() {
+        expect(false).to.equal(false);
+      });
+  });
