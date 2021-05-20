a9366fa1eab23673f67f78cb65a55ed47a58c5df
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-05-20 17:06:27 +0200

    detail

--- Assignment/client/src/app/kind/add-kind/add-kind.component.ts
+++ Assignment/client/src/app/kind/add-kind/add-kind.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { KindDataService } from '../kind-data.service';
 import { Kind } from '../kind.model';
 
