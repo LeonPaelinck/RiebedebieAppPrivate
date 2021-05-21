679c5799e75e77e4759b0b1b0588468b186a5216
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-05-21 18:55:48 +0200

    login systeem forntend

--- Assignment/client/src/app/user/login/login.component.ts
+++ Assignment/client/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user: FormGroup;
+  public errorMessage: string = '';
+
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.user = this.fb.group({
+      username: ['parent@stekene.be', Validators.required],
+      password: ['P@ssword1111', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.authService.login(this.user.value.username, 
+            this.user.value.password).subscribe(val => {
+      if (val) {
+        if (this.authService.redirectUrl) {
+          this.router.navigateByUrl(this.authService.redirectUrl);
+          this.authService.redirectUrl = undefined;
+        } else {
+          this.router.navigate(['/kind/list']);
+        }
+      }
+    }, err => this.errorMessage = err.json().message);
   }
 
 }
