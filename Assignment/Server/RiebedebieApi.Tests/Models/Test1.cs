f7f35fb9720da583cc5d2604d32a09a806b6b795
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-07-13 00:16:03 +0200

    Backend Test Project toegevoegd

---
+++ Assignment/Server/RiebedebieApi.Tests/Models/Test1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RiebedebieApi.Tests.Models
+{
+    public class Test1
+    {
+        [Fact]
+        public void SlaagtAltijd()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+            Assert.True(true);
+        }
+    }
+}
