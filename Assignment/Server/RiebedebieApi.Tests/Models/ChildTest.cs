2a66531778cc9de10c3d6f4af4d3c795690b9ca2
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-07-13 14:30:25 +0200

    model test

---
+++ Assignment/Server/RiebedebieApi.Tests/Models/ChildTest.cs
@@ -0,0 +1,36 @@
+﻿using RiebedebieApi.Models;
+using System;
+using Xunit;
+
+namespace RiebedebieApi.Tests.Models
+{
+    public class ChildTest
+    {
+        public static string voornaam = "leon";
+        public static string familienaam = "paelinck";
+
+        [Fact]
+        public void NieuwChild_CorrectChild_MaaktChild()
+        {
+            //Arrange
+            DateTime geboortedatum = DateTime.Today.AddYears(-10);
+            //Act
+            Child c = new Child(familienaam, voornaam, geboortedatum);
+            //Assert
+            Assert.Equal(voornaam, c.FirstName);
+            Assert.Equal(familienaam, c.LastName);
+            Assert.Equal(geboortedatum, c.BirthDate);
+            Assert.Equal(AgeCategory.Teenager, c.AgeCategory); //zou zelf meoten berekenene 
+        }
+
+        [Fact]
+        public void NieuwChild_IncorrectChild_MaakGeentChild()
+        {
+            //Arrange
+            DateTime toekomst = DateTime.Today.AddYears(10);
+            //Act
+            //Assert
+            Assert.Throws<ArgumentException>(() => new Child(familienaam, voornaam, toekomst));
+        }
+    }
+}
