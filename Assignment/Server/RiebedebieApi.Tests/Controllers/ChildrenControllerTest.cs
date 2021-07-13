e7f7ef445a37802a4997b0038a2208f90da1fe91
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-07-13 17:15:00 +0200

    Putchild test

---
+++ Assignment/Server/RiebedebieApi.Tests/Controllers/ChildrenControllerTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using RecipeApi.DTOs;
+using RiebedebieApi.Controllers;
+using RiebedebieApi.Models;
+using RiebedebieApi.Tests.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RiebedebieApi.Tests.Models
+{
+    public class ChildrenControllerTest
+    {
+        private readonly ChildrenController _childrenController;
+        private readonly DummyApplicationDbContext _dbContext;
+
+        private readonly Mock<IChildRepository> _mockChildRepo;
+        private readonly Mock<IParentRepository> _mockParentRepo;
+
+        private readonly Mock<UserManager<IdentityUser>> _mockUserManager;
+
+        public ChildrenControllerTest()
+        {
+            _dbContext = new DummyApplicationDbContext();
+            //create mocks
+            _mockChildRepo = new Mock<IChildRepository>();
+            _mockParentRepo = new Mock<IParentRepository>();
+            //inject mocks
+            _childrenController = new ChildrenController(_mockChildRepo.Object, _mockParentRepo.Object);
+        }
+
+        #region == Create Methodes ==
+        [Fact]
+        public void PostChildSuccessful()
+        {
+            //Mock
+            _mockParentRepo.Setup(m => m.GetBy(It.IsNotNull<string>())).Returns(_dbContext.Parent1);
+            //Arrange
+            ChildDTO childDTO = new ChildDTO();
+            childDTO.BirthDate = _dbContext.Kind.BirthDate.ToString();
+            childDTO.FirstName = _dbContext.Kind.FirstName;
+            childDTO.LastName = _dbContext.Kind.LastName;
+            //Act
+            var result = Assert.IsType<ActionResult<Child>>(_childrenController.PostChild(childDTO));
+            //Assert
+            Child child = result.Value;
+            //Verify
+            _mockChildRepo.Verify(m => m.Add(It.IsNotNull<Child>()), Times.Once);
+            _mockParentRepo.Verify(m => m.GetBy(It.IsNotNull<string>()), Times.Once);
+            _mockChildRepo.Verify(m => m.SaveChanges(), Times.Once);
+
+        }
+
+        #endregion
+
+        #region == Update Methodes ==
+        [Fact]
+        public void PutChildSuccessful()
+        {
+            //Arrange
+            Child child = _dbContext.Kleuter;
+            //Act/Assert
+            var result = Assert.IsType<NoContentResult>(_childrenController.PutChild(child.Id, child));
+            //Verify
+            _mockChildRepo.Verify(m => m.Update(It.IsNotNull<Child>()), Times.Once);
+            _mockChildRepo.Verify(m => m.SaveChanges(), Times.Once);
+
+        }
+        #endregion
+    }
+}
--- Assignment/Server/RiebedebieApi.Tests/Data/DummyApplicationDbContext.cs
+++ Assignment/Server/RiebedebieApi.Tests/Data/DummyApplicationDbContext.cs
@@ -6,15 +6,17 @@ namespace RiebedebieApi.Tests.Data
     public class DummyApplicationDbContext
     {
         public Child Kleuter { get; set; }
-
         public Child Kind { get; set; }
         public Child Tiener { get; set; }
+        public Parent Parent1 { get; private set; }
 
         public DummyApplicationDbContext()
         {
             Kleuter = new Child("Joosens", "Set", DateTime.Now.AddYears(-4));
             Kind = new Child("Joosens", "Chris", DateTime.Now.AddYears(-8));
             Tiener = new Child("Joosens", "Staf", DateTime.Now.AddYears(-13));
+
+            Parent1 = new Parent { Email = "parent@stekene.be", FirstName = "John", LastName = "Doe" };
 
         }
     }
